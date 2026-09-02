9d992e9f6a7709c8103250cdcda3abceb23126bd
xt9y <felix@xt9y.de>
2026-09-02 13:20:34 +0200

    feat: expose local tunnel cleanup

--- include/opal/tunnel.hpp
+++ include/opal/tunnel.hpp
@@ -5,5 +5,6 @@ int tunnel_host_setup(std::string &connection_code);
 int tunnel_host_start();
 bool tunnel_access(const std::string &control_token,const std::string &video_token);
 bool tunnel_connection_code(const std::string &code,std::string *control_token=nullptr,std::string *video_token=nullptr);
+int tunnel_clean_local();
 int tunnel_host();
 }
