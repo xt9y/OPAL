d77a695804bca77223b0d276f12ad07a1631034b
xt9y <felix@xt9y.de>
2026-09-02 20:38:43 +0200

    feat: add SessionSupervisor interface

---
+++ include/opal/session.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <functional>
+#include <memory>
+#include <string>
+
+namespace opal {
+struct SessionOptions {
+    std::string target;
+    int control_port=47990;
+    int video_port=47991;
+    bool tunneled=false;
+    std::string control_token;
+    std::string video_token;
+    std::string fingerprint;
+    std::string client_public_key;
+    std::string client_private_key_path;
+    bool paired=false;
+    std::string pairing_password;
+    std::function<std::string()> pairing_password_provider;
+    std::string label;
+};
+
+class SessionSupervisor {
+public:
+    explicit SessionSupervisor(SessionOptions options);
+    ~SessionSupervisor();
+    SessionSupervisor(const SessionSupervisor&)=delete;
+    SessionSupervisor& operator=(const SessionSupervisor&)=delete;
+
+    bool start();
+    void stop();
+    bool send_input(const std::string &command);
+    unsigned long control_generation() const;
+    bool media_started() const;
+    bool running() const;
+    bool paired() const;
+    std::string fingerprint() const;
+    std::string last_error() const;
+
+private:
+    struct Impl;
+    std::unique_ptr<Impl> impl_;
+};
+}
