8e3280f8df0242dac3299b8ad7da173785a8025d
xt9y <felix@xt9y.de>
2026-09-02 15:53:31 +0200

    feat: define supervised capture process

--- include/opal/media.hpp
+++ include/opal/media.hpp
@@ -1,3 +1,15 @@
 #pragma once
+#include <cstddef>
 #include <string>
-namespace opal { std::string capture_command(bool gpu_screen_recorder,int fps,int bitrate_kbps,bool audio); }
+#include <sys/types.h>
+
+namespace opal {
+struct CaptureProcess {
+    pid_t pid=-1;
+    int fd=-1;
+};
+std::string capture_command(bool gpu_screen_recorder,int fps,int bitrate_kbps,bool audio);
+CaptureProcess start_capture(const std::string &command);
+int read_capture(CaptureProcess &capture,void *buffer,size_t size,int timeout_ms);
+void stop_capture(CaptureProcess &capture);
+}
