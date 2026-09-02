81a0cebc6bdfb2de893047582b850e5c5b6975b2
xt9y <felix@xt9y.de>
2026-09-02 15:08:38 +0200

    fix: stream GSR video to stdout

--- src/media.cpp
+++ src/media.cpp
@@ -17,7 +17,7 @@ std::string capture_command(bool gsr,int fps,int bitrate,bool audio){
             " -k h264 -fallback-cpu-encoding yes -bm cbr -q "+std::to_string(bitrate)+
             " -v no"+
             (audio?" -a default_output":"")+
-            " -c flv -o -"+
+            " -c flv"+
             (debug_enabled?"":" 2>/dev/null");
     }
     return "ffmpeg -hide_banner -loglevel error -f x11grab -draw_mouse 1 -framerate "+std::to_string(fps)+" -i ${DISPLAY:-:0.0} "+(audio?"-f pulse -i default ":"")+"-c:v libx264 -preset ultrafast -tune zerolatency -b:v "+std::to_string(bitrate)+"k -maxrate "+std::to_string(bitrate)+"k -bufsize "+std::to_string(bitrate)+"k "+(audio?"-c:a aac -b:a 128k ":"")+"-f flv pipe:1";
