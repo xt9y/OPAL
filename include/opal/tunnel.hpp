36cdcfbd12278d4746e4b37ba2ca5a5f9cb9363f
xt9y <felix@xt9y.de>
2026-09-02 11:51:34 +0200

    feat: define persistent tunnel-only transport

--- include/opal/tunnel.hpp
+++ include/opal/tunnel.hpp
@@ -1,3 +1,9 @@
 #pragma once
 #include <string>
-namespace opal { int tunnel_host(); bool tunnel_access(const std::string &control_token,const std::string &video_token); }
+namespace opal {
+int tunnel_host_setup(std::string &connection_code);
+int tunnel_host_start();
+bool tunnel_access(const std::string &control_token,const std::string &video_token);
+bool tunnel_connection_code(const std::string &code,std::string *control_token=nullptr,std::string *video_token=nullptr);
+int tunnel_host();
+}
