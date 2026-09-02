4b765dbaed29c53b9090b6c8d465558869391833
xt9y <felix@xt9y.de>
2026-09-02 16:42:22 +0200

    feat: restore OPAL portal and repeated sessions

--- include/opal/media.hpp
+++ include/opal/media.hpp
@@ -8,7 +8,7 @@ struct CaptureProcess {
     pid_t pid=-1;
     int fd=-1;
 };
-std::string capture_command(bool gpu_screen_recorder,int fps,int bitrate_kbps,bool audio);
+std::string capture_command(bool gpu_screen_recorder,int fps,int bitrate_kbps,bool audio,const std::string &portal_token_file="");
 CaptureProcess start_capture(const std::string &command);
 int read_capture(CaptureProcess &capture,void *buffer,size_t size,int timeout_ms);
 void stop_capture(CaptureProcess &capture);
