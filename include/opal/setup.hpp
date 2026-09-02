b00e5dbdbb837ec8ddd1c5ffae0ae10ce38d3f40
xt9y <felix@xt9y.de>
2026-09-03 00:47:11 +0200

    feat: add simple host selection commands

--- include/opal/setup.hpp
+++ include/opal/setup.hpp
@@ -1,2 +1,7 @@
 #pragma once
-namespace opal { int interactive_run(); int interactive_setup(); }
+namespace opal {
+int interactive_run();
+int interactive_setup();
+int interactive_select();
+int interactive_remove();
+}
