d3fc79212c284cba41509d938c17363bf9d4a470
xt9y <felix@xt9y.de>
2026-09-02 19:03:36 +0200

    feat: declare OPAL service restart

--- include/opal/system.hpp
+++ include/opal/system.hpp
@@ -1,2 +1,2 @@
 #pragma once
-namespace opal { int init(); int doctor(); int host_service(bool enable); int clean(); int bridge_setup(const char *mac); }
+namespace opal { int init(); int doctor(); int host_service(bool enable); int restart_services(); int clean(); int bridge_setup(const char *mac); }
