f2002c00b11e8d53b9bf441078f77f8c4d80af85
xt9y <felix@xt9y.de>
2026-09-03 12:36:23 +0200

    feat: add direct video sender interface

---
+++ include/opal/video_sender.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <opal/direct_video_session.hpp>
+#include <opal/media_profile.hpp>
+#include <atomic>
+#include <cstddef>
+#include <cstdint>
+#include <functional>
+#include <memory>
+#include <string>
+
+namespace opal {
+
+class VideoSender {
+public:
+    VideoSender();
+    VideoSender(const VideoSender&)=delete;
+    VideoSender& operator=(const VideoSender&)=delete;
+    bool start(DirectVideoPath path,const StreamOptions& stream,bool audio,
+               std::function<void(const std::string&)> control_send);
+    void request_idr();
+    void set_target_bitrate(int kbps);
+    int target_bitrate() const;
+    std::size_t queued_frames() const;
+    std::size_t queued_bytes() const;
+    std::uint64_t stale_frames() const;
+    void stop();
+    ~VideoSender();
+private:
+    struct Impl;
+    std::unique_ptr<Impl> impl_;
+};
+
+}
