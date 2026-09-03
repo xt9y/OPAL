e750c0ee4d58c5ccf76c68c223853c84b9ff28df
xt9y <felix@xt9y.de>
2026-09-03 18:49:15 +0200

    perf: expose borrowed encoded media view

--- include/opal/video_capture.hpp
+++ include/opal/video_capture.hpp
@@ -2,43 +2,24 @@
 #include <opal/media_profile.hpp>
 #include <cstdint>
 #include <memory>
+#include <span>
 #include <string>
 #include <vector>
 
 namespace opal {
 enum class MediaKind { VideoH264, AudioAac };
-
-struct MediaConfig {
-    MediaKind kind=MediaKind::VideoH264;
-    std::vector<std::uint8_t> extradata;
-    int sample_rate=0;
-    int channels=0;
-};
-
-struct EncodedMediaUnit {
-    MediaKind kind=MediaKind::VideoH264;
-    std::vector<std::uint8_t> data;
-    std::int64_t pts_us=0;
-    std::uint64_t capture_time_us=0;
-    bool keyframe=false;
-};
-
+struct MediaConfig {MediaKind kind=MediaKind::VideoH264;std::vector<std::uint8_t> extradata;int sample_rate=0;int channels=0;};
+struct EncodedMediaView {MediaKind kind=MediaKind::VideoH264;std::span<const std::uint8_t> data;std::int64_t pts_us=0;std::uint64_t capture_time_us=0;bool keyframe=false;};
+struct EncodedMediaUnit {MediaKind kind=MediaKind::VideoH264;std::vector<std::uint8_t> data;std::int64_t pts_us=0;std::uint64_t capture_time_us=0;bool keyframe=false;};
 class VideoCapture {
 public:
-    VideoCapture();
-    ~VideoCapture();
-    VideoCapture(const VideoCapture&)=delete;
-    VideoCapture& operator=(const VideoCapture&)=delete;
-
-    bool start(const StreamOptions &stream,int bitrate_kbps,bool audio,
-               const std::string &portal_token_file);
+    VideoCapture();~VideoCapture();VideoCapture(const VideoCapture&)=delete;VideoCapture& operator=(const VideoCapture&)=delete;
+    bool start(const StreamOptions &stream,int bitrate_kbps,bool audio,const std::string &portal_token_file);
+    bool next_view(EncodedMediaView &unit,int timeout_ms);
     bool next(EncodedMediaUnit &unit,int timeout_ms);
     bool ended() const;
     const std::vector<MediaConfig>& configs() const;
     void stop();
-
-private:
-    struct Impl;
-    std::unique_ptr<Impl> impl_;
+private:struct Impl;std::unique_ptr<Impl> impl_;
 };
 }
