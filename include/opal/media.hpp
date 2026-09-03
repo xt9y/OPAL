8e08601c10b8eb790661a5edcd9785209257e0a8
xt9y <felix@xt9y.de>
2026-09-03 13:04:02 +0200

    refactor: remove obsolete video transport API

--- include/opal/media.hpp
+++ include/opal/media.hpp
@@ -8,14 +8,11 @@
 namespace opal {
 struct CaptureProcess { pid_t pid=-1; int fd=-1; };
 struct SinkProcess { pid_t pid=-1; int fd=-1; };
-std::string video_request_line(const std::string &token,int max_width,int max_height,int fps);
-bool parse_video_request_line(const std::string &line,std::string &token,int &max_width,int &max_height,int &fps);
 std::string capture_command(bool gpu_screen_recorder,int fps,int bitrate_kbps,bool audio,const std::string &portal_token_file="",int max_width=0,int max_height=0);
 CaptureProcess start_capture(const std::string &command);
 int read_capture(CaptureProcess &capture,void *buffer,size_t size,int timeout_ms);
 void stop_capture(CaptureProcess &capture);
 SinkProcess start_sink(const std::string &command);
-int video_player_write_timeout_ms();
 bool write_sink_timeout(SinkProcess &sink,const void *data,size_t size,int timeout_ms);
 void stop_sink(SinkProcess &sink);
 }
