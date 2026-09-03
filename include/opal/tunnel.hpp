5cc860457f85ea312bc460436e6dc97b123546f4
xt9y <felix@xt9y.de>
2026-09-03 12:48:44 +0200

    refactor: define control-only tunnel interface

--- include/opal/tunnel.hpp
+++ include/opal/tunnel.hpp
@@ -7,8 +7,8 @@ int tunnel_host_start();
 bool tunnel_host_healthy();
 int tunnel_host_ensure_running();
 void tunnel_host_supervise(std::atomic<bool> &run,int interval_ms=1000);
-bool tunnel_access(const std::string &control_token,const std::string &video_token,int timeout_ms=30000);
-bool tunnel_connection_code(const std::string &code,std::string *control_token=nullptr,std::string *video_token=nullptr);
+bool tunnel_access(const std::string &control_token,int timeout_ms=30000);
+bool tunnel_connection_code(const std::string &code,std::string *control_token=nullptr,std::string *legacy_video_token=nullptr);
 int tunnel_clean_local();
 int tunnel_host();
 }
