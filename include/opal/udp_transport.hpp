9d2e648fa2d604b4ccd55e9c8bbfc189facf6b7c
xt9y <felix@xt9y.de>
2026-09-04 00:26:36 +0200

    OPAL: add bounded UDP batch receive API

--- include/opal/udp_transport.hpp
+++ include/opal/udp_transport.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstddef>
 #include <cstdint>
 #include <optional>
 #include <span>
@@ -9,6 +10,7 @@
 namespace opal {
 constexpr int kUdpQueueBufferBytes=64*1024;
 constexpr int kUdpInteractiveTrafficClass=0xb8; // DSCP EF, ECN bits clear
+constexpr std::size_t kUdpReceiveBatchMax=32;
 
 enum class CandidateType { Local, ServerReflexive };
 
@@ -28,6 +30,14 @@ struct UdpSocket {
     std::uint16_t local_port=0;
 };
 
+struct UdpReceiveSlot {
+    std::span<std::uint8_t> buffer{};
+    sockaddr_storage source{};
+    socklen_t source_length=0;
+    std::size_t size=0;
+    std::uint32_t kernel_drops=0;
+};
+
 UdpSocket open_udp_socket();
 void close_udp_socket(UdpSocket&);
 std::vector<UdpCandidate> local_udp_candidates(const UdpSocket&);
@@ -37,4 +47,5 @@ std::optional<UdpCandidate> discover_server_reflexive_candidate(
 bool resolve_udp_endpoint(const std::string&,std::uint16_t,sockaddr_storage&,socklen_t&);
 bool send_datagram(int,const sockaddr_storage&,socklen_t,std::span<const std::uint8_t>);
 int recv_datagram(int,std::span<std::uint8_t>,sockaddr_storage&,socklen_t&,int timeout_ms);
+int recv_datagrams_batch(int,std::span<UdpReceiveSlot>,int timeout_ms);
 }
