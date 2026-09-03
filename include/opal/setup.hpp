b764522274ba816e3e54435cea4ac2f972591150
xt9y <felix@xt9y.de>
2026-09-03 02:30:58 +0200

    feat: pass stream options through setup

--- include/opal/setup.hpp
+++ include/opal/setup.hpp
@@ -1,6 +1,7 @@
 #pragma once
+#include <opal/media.hpp>
 namespace opal {
-int interactive_run();
+int interactive_run(const StreamOptions &stream={});
 int interactive_setup();
 int interactive_select();
 int interactive_remove();
