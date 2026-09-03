4f5757f6504f5e1af354da4d5e0b36d3be6c5ac0
xt9y <felix@xt9y.de>
2026-09-03 02:31:27 +0200

    feat: expose per-run client stream options

--- include/opal/client.hpp
+++ include/opal/client.hpp
@@ -1,3 +1,9 @@
 #pragma once
+#include <opal/media.hpp>
 #include <string>
-namespace opal { int client_connect(const std::string &target,const std::string &password=""); int hosts_add(const std::string &name,const std::string &address,const std::string &mac=""); int hosts_list(); }
+namespace opal {
+int client_connect(const std::string &target,const std::string &password="");
+int client_connect(const std::string &target,const std::string &password,const StreamOptions &stream);
+int hosts_add(const std::string &name,const std::string &address,const std::string &mac="");
+int hosts_list();
+}
