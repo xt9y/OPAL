11e891cfd96b3dec7a37787ce0b74bee76935075
xt9y <felix@xt9y.de>
2026-09-03 12:47:31 +0200

    refactor: make client zrok access control-only

--- include/opal/tunnel_access.hpp
+++ include/opal/tunnel_access.hpp
@@ -5,11 +5,9 @@
 namespace opal {
 struct TunnelAccessHandle {
     pid_t control_pid=-1;
-    pid_t video_pid=-1;
     int control_port=0;
-    int video_port=0;
 };
-bool tunnel_access_start(TunnelAccessHandle &handle,const std::string &control_token,const std::string &video_token,int timeout_ms=30000);
+bool tunnel_access_start(TunnelAccessHandle &handle,const std::string &control_token,int timeout_ms=30000);
 bool tunnel_access_healthy(TunnelAccessHandle &handle);
 void tunnel_access_stop(TunnelAccessHandle &handle);
 }
