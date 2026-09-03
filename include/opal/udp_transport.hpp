043f810c1406a8f868f017554c21f8f951a0c34a
xt9y <felix@xt9y.de>
2026-09-04 01:50:48 +0200

    OPAL: remove STUN transport API

--- include/opal/udp_transport.hpp
+++ include/opal/udp_transport.hpp
@@ -1,7 +1,6 @@
 #pragma once
 #include <cstddef>
 #include <cstdint>
-#include <optional>
 #include <span>
 #include <string>
 #include <vector>
@@ -12,17 +11,9 @@ constexpr int kUdpQueueBufferBytes=64*1024;
 constexpr int kUdpInteractiveTrafficClass=0xb8; // DSCP EF, ECN bits clear
 constexpr std::size_t kUdpReceiveBatchMax=32;
 
-enum class CandidateType { Local, ServerReflexive };
-
-struct StunEndpoint {
-    std::string host;
-    std::uint16_t port=0;
-};
-
 struct UdpCandidate {
     std::string host;
     std::uint16_t port=0;
-    CandidateType type=CandidateType::Local;
 };
 
 struct UdpSocket {
@@ -41,9 +32,6 @@ struct UdpReceiveSlot {
 UdpSocket open_udp_socket();
 void close_udp_socket(UdpSocket&);
 std::vector<UdpCandidate> local_udp_candidates(const UdpSocket&);
-std::vector<StunEndpoint> default_stun_endpoints();
-std::optional<UdpCandidate> discover_server_reflexive_candidate(
-    const UdpSocket&,const std::vector<StunEndpoint>&,int timeout_ms);
 bool resolve_udp_endpoint(const std::string&,std::uint16_t,sockaddr_storage&,socklen_t&);
 bool send_datagram(int,const sockaddr_storage&,socklen_t,std::span<const std::uint8_t>);
 int recv_datagram(int,std::span<std::uint8_t>,sockaddr_storage&,socklen_t&,int timeout_ms);
