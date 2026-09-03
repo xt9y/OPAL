6fb82e9c0af21658311522753a00eb54e259d4f3
xt9y <felix@xt9y.de>
2026-09-03 11:15:36 +0200

    refactor: separate media profile from process helpers

--- include/opal/media.hpp
+++ include/opal/media.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <opal/media_profile.hpp>
 #include <chrono>
 #include <cstddef>
 #include <string>
@@ -7,9 +8,6 @@
 namespace opal {
 struct CaptureProcess { pid_t pid=-1; int fd=-1; };
 struct SinkProcess { pid_t pid=-1; int fd=-1; };
-struct StreamOptions { int max_width=0; int max_height=0; int fps=60; };
-bool stream_mode_limit(const std::string &mode,int &max_width,int &max_height);
-int automatic_bitrate_kbps(int width,int height,int fps);
 std::string video_request_line(const std::string &token,int max_width,int max_height,int fps);
 bool parse_video_request_line(const std::string &line,std::string &token,int &max_width,int &max_height,int &fps);
 std::string capture_command(bool gpu_screen_recorder,int fps,int bitrate_kbps,bool audio,const std::string &portal_token_file="",int max_width=0,int max_height=0);
