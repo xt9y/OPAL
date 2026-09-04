5559298b1c0b0f12db21fdfc0aba09ad49736c06
xt9y <felix@xt9y.de>
2026-09-04 12:31:12 +0200

    test: require dependency-preserving decode backlog

--- tests/test_video_receiver_architecture.cpp
+++ tests/test_video_receiver_architecture.cpp
@@ -13,10 +13,16 @@ int main(){
     assert(source.find("media_thread")!=std::string::npos);
     assert(source.find("media_cv")!=std::string::npos);
     assert(source.find("recv_datagrams_batch")!=std::string::npos);
-    assert(source.find("std::optional<MediaItem> video_frame")!=std::string::npos);
-    assert(source.find("std::optional<MediaItem> video_config")!=std::string::npos);
+    assert(source.find("kVideoDecodeBacklogCapacity=8")!=std::string::npos);
+    assert(source.find("std::array<std::optional<MediaItem>,kVideoDecodeBacklogCapacity> video_frames")!=std::string::npos);
+    assert(source.find("std::optional<MediaItem> video_frame")==std::string::npos);
     assert(source.find("std::deque<MediaItem>")==std::string::npos);
-    assert(source.find("video_frame.reset()")!=std::string::npos);
+    assert(source.find("clear_video_backlog")!=std::string::npos);
+    assert(source.find("push_video_backlog")!=std::string::npos);
+    assert(source.find("pop_video_backlog")!=std::string::npos);
+    assert(source.find("video_backlog_size")!=std::string::npos);
+    assert(source.find("decode-backlog")!=std::string::npos);
+    assert(source.find("skip_present")!=std::string::npos);
     assert(source.find("request_idr_rx")!=std::string::npos);
     return 0;
 }
