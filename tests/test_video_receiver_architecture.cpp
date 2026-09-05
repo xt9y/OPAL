ca00468bf9559d62ff5ac64f1a931e4ced4a8842
xt9y <felix@xt9y.de>
2026-09-05 02:48:45 +0200

    HPI: replace receiver source-shape test with backlog behavior

--- tests/test_video_receiver_architecture.cpp
+++ tests/test_video_receiver_architecture.cpp
@@ -1,28 +1,29 @@
+#include <opal/video_backlog.hpp>
+
 #include <cassert>
-#include <fstream>
-#include <iterator>
 #include <string>
 
-static std::string read_all(const char *path){std::ifstream in(path);assert(in.good());return std::string(std::istreambuf_iterator<char>(in),std::istreambuf_iterator<char>());}
-
 int main(){
-    const auto source=read_all("src/video_receiver.cpp");const auto header=read_all("include/opal/video_receiver.hpp");
-    assert(source.find("media_thread")!=std::string::npos);
-    assert(source.find("media_cv")!=std::string::npos);
-    assert(source.find("recv_datagrams_batch")!=std::string::npos);
-    assert(source.find("kVideoDecodeBacklogCapacity=8")!=std::string::npos);
-    assert(source.find("std::array<std::optional<MediaItem>,kVideoDecodeBacklogCapacity>video_frames")!=std::string::npos);
-    assert(source.find("clear_video_backlog")!=std::string::npos);
-    assert(source.find("push_video_backlog")!=std::string::npos);
-    assert(source.find("pop_video_backlog")!=std::string::npos);
-    assert(source.find("decode-backlog")!=std::string::npos);
-    assert(source.find("publish_latest")!=std::string::npos);
-    assert(source.find("av_frame_clone")!=std::string::npos);
-    assert(source.find("DecodedVideoFrame latest_frame")!=std::string::npos);
-    assert(source.find("take_latest")!=std::string::npos);
-    assert(source.find("VideoPresenter")==std::string::npos);
-    assert(source.find("present_borrowed")==std::string::npos);
-    assert(header.find("take_latest_video")!=std::string::npos);
-    assert(header.find("presentation_window")==std::string::npos);
+    opal::VideoBacklog<std::string,2> backlog;
+    assert(backlog.size()==0);
+    assert(backlog.push("p1",false)==opal::VideoBacklogPush::Queued);
+    assert(backlog.push("p2",false)==opal::VideoBacklogPush::Queued);
+    assert(backlog.size()==2);
+
+    assert(backlog.push("p3",false)==opal::VideoBacklogPush::DroppedIncoming);
+    assert(backlog.size()==2);
+    auto first=backlog.pop();auto second=backlog.pop();
+    assert(first&&*first=="p1");
+    assert(second&&*second=="p2");
+    assert(!backlog.pop());
+
+    assert(backlog.push("old1",false)==opal::VideoBacklogPush::Queued);
+    assert(backlog.push("old2",false)==opal::VideoBacklogPush::Queued);
+    assert(backlog.push("idr",true)==opal::VideoBacklogPush::ReplacedBacklog);
+    assert(backlog.size()==1);
+    auto idr=backlog.pop();
+    assert(idr&&*idr=="idr");
+    assert(backlog.size()==0);
+
     return 0;
 }
