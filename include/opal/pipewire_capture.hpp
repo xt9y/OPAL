19808c5b988935d2b9fe139b27280808c3f5ecdf
xt9y <felix@xt9y.de>
2026-09-05 21:26:39 +0200

    HPI: add native PipeWire capture interface

---
+++ include/opal/pipewire_capture.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <opal/media_profile.hpp>
+#include <opal/video_capture.hpp>
+#include <cstdint>
+#include <memory>
+#include <string>
+
+namespace opal {
+
+class NativePipeWireVideoCapture {
+public:
+    NativePipeWireVideoCapture();
+    NativePipeWireVideoCapture(const NativePipeWireVideoCapture&)=delete;
+    NativePipeWireVideoCapture& operator=(const NativePipeWireVideoCapture&)=delete;
+    bool start(const StreamOptions& stream,int bitrate_kbps,const std::string& restore_token_file);
+    bool next(EncodedMediaUnit& unit,int timeout_ms);
+    bool ended() const;
+    std::uint64_t config_revision() const;
+    MediaConfig config() const;
+    std::string backend_name() const;
+    std::string last_error() const;
+    void stop();
+    ~NativePipeWireVideoCapture();
+    static bool compiled();
+private:
+    struct Impl;
+    std::unique_ptr<Impl> impl_;
+};
+
+}
