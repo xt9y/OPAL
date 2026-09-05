123f32a540fe053bdb6ffa3f4692b0b5246e355e
xt9y <felix@xt9y.de>
2026-09-05 23:39:20 +0200

    refactor: make video presenter SDL-only

--- include/opal/video_present.hpp
+++ include/opal/video_present.hpp
@@ -15,7 +15,6 @@ public:
     VideoPresenter(const VideoPresenter&)=delete;
     VideoPresenter& operator=(const VideoPresenter&)=delete;
     static bool supports_cpu_upload_format(int av_pixel_format);
-    static bool drm_prime_supported_format(int av_pixel_format);
     bool open(int source_width,int source_height,bool fullscreen=true,int source_format=-1);
     bool present_borrowed(DecodedVideoView frame);
     bool present(DecodedVideoFrame frame);
