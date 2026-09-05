dcecf0a2c5c1ceec4ef5ea12840a6e7d6a1560d4
xt9y <felix@xt9y.de>
2026-09-05 23:10:33 +0200

    HPI: select presenter from decoded format

--- include/opal/video_present.hpp
+++ include/opal/video_present.hpp
@@ -16,7 +16,7 @@ public:
     VideoPresenter& operator=(const VideoPresenter&)=delete;
     static bool supports_cpu_upload_format(int av_pixel_format);
     static bool drm_prime_supported_format(int av_pixel_format);
-    bool open(int source_width,int source_height,bool fullscreen=true);
+    bool open(int source_width,int source_height,bool fullscreen=true,int source_format=-1);
     bool present_borrowed(DecodedVideoView frame);
     bool present(DecodedVideoFrame frame);
     std::pair<int,int> drawable_size() const;
