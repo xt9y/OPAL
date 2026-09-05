33e9334cbd8628ddff45af73424dd6924fc64f26
xt9y <felix@xt9y.de>
2026-09-05 23:09:04 +0200

    HPI: expose low-latency H264 slice profile

--- include/opal/media_profile.hpp
+++ include/opal/media_profile.hpp
@@ -16,6 +16,7 @@ int automatic_stream_fps(int display_refresh_hz,int capture_max_fps);
 int automatic_bitrate_kbps(int width,int height,int fps);
 std::uint64_t capture_stale_budget_us(int fps);
 int normal_gop_frames(int fps);
+int low_latency_h264_slices(int width,int height);
 std::uint64_t sender_burst_budget_bytes(int bitrate_kbps,int fps,bool keyframe);
 int sender_pacing_rate_kbps(int bitrate_kbps,bool keyframe);
 int encoder_reconfigure_bitrate_kbps(int active_kbps,int target_kbps,std::uint64_t since_restart_ms);
