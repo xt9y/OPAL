b6a9f17d59d3b00491938398a559c410f2cbe588
xt9y <felix@xt9y.de>
2026-09-05 02:54:48 +0200

    HPI: add sender pacing lock regression

---
+++ tests/test_media_pacer.cpp
@@ -0,0 +1,21 @@
+#include <opal/media_pacer.hpp>
+
+#include <cassert>
+#include <cstdint>
+
+int main(){
+    opal::MediaTokenBucket pacer;
+    pacer.reset(1200.0,1000000);
+    assert(pacer.reserve_or_delay_us(1200,1200.0,8000,1000000)==0);
+    const auto wait=pacer.reserve_or_delay_us(1200,1200.0,8000,1000000);
+    assert(wait>=1199&&wait<=1201);
+    assert(pacer.reserve_or_delay_us(1200,1200.0,8000,1001200)==0);
+
+    pacer.reset(2400.0,2000000);
+    assert(pacer.reserve_or_delay_us(1200,2400.0,8000,2000000)==0);
+    assert(pacer.reserve_or_delay_us(1200,2400.0,8000,2000000)==0);
+    assert(pacer.reserve_or_delay_us(1,2400.0,8000,2000000)>=1);
+    pacer.top_up(2400.0,2000000);
+    assert(pacer.reserve_or_delay_us(2400,2400.0,8000,2000000)==0);
+    return 0;
+}
