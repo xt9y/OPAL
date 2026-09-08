7f13987549a60707563c1521e48aef6b5398fed6
xt9y <felix@xt9y.de>
2026-09-08 03:19:28 +0200

    feat: add capture backend contract

---
+++ include/opal/capture_backend.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <opal/media_profile.hpp>
+#include <opal/native_video_frame.hpp>
+#include <opal/platform_error.hpp>
+#include <opal/video_capture.hpp>
+
+#include <memory>
+#include <string>
+
+namespace opal {
+
+class CaptureBackend {
+public:
+    virtual ~CaptureBackend() = default;
+    virtual bool start(const StreamOptions &stream) = 0;
+    virtual bool next(NativeVideoFrame &frame, int timeout_ms) = 0;
+    virtual void stop() = 0;
+    virtual CaptureTimestampQuality timestamp_quality() const = 0;
+    virtual std::string backend_name() const = 0;
+    virtual PlatformError last_platform_error() const = 0;
+};
+
+std::unique_ptr<CaptureBackend> make_capture_backend();
+
+}
