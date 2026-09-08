4a32dde33e48d31fcc695e55d0e2db95f96c07e7
xt9y <felix@xt9y.de>
2026-09-08 11:10:15 +0200

    test: require VideoToolbox low latency rate control

--- tests/test_macos_videotoolbox_contract.cpp
+++ tests/test_macos_videotoolbox_contract.cpp
@@ -16,10 +16,12 @@ int main()
     const auto text = read_all("src/platform/macos/video_encoder_backend.mm");
     assert(text.find("VTCompressionSessionRef") != std::string::npos);
     assert(text.find("kVTVideoEncoderSpecification_RequireHardwareAcceleratedVideoEncoder") != std::string::npos);
+    assert(text.find("kVTVideoEncoderSpecification_EnableLowLatencyRateControl") != std::string::npos);
     assert(text.find("kVTCompressionPropertyKey_RealTime") != std::string::npos);
     assert(text.find("kVTCompressionPropertyKey_AllowFrameReordering") != std::string::npos);
     assert(text.find("kVTCompressionPropertyKey_AverageBitRate") != std::string::npos);
     assert(text.find("kVTCompressionPropertyKey_ExpectedFrameRate") != std::string::npos);
+    assert(text.find("kVTCompressionPropertyKey_MaxKeyFrameInterval") == std::string::npos);
     assert(text.find("kVTEncodeFrameOptionKey_ForceKeyFrame") != std::string::npos);
     assert(text.find("CMVideoFormatDescriptionGetH264ParameterSetAtIndex") != std::string::npos);
     assert(text.find("std::shared_ptr<void> pixel_owner") != std::string::npos);
