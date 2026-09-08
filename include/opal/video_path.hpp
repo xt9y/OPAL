2ab1902167c72c452bd89c995161cc7e4fd96aee
xt9y <felix@xt9y.de>
2026-09-08 03:04:42 +0200

    refactor: remove native socket address from video path

--- include/opal/video_path.hpp
+++ include/opal/video_path.hpp
@@ -9,8 +9,7 @@ namespace opal {
 
 struct DirectVideoPath {
     UdpSocket socket;
-    sockaddr_storage peer{};
-    socklen_t peer_len=0;
+    UdpEndpoint peer{};
     VideoKeys keys;
     std::uint64_t session_id=0;
     std::uint32_t generation=0;
@@ -24,7 +23,7 @@ struct DirectVideoPath {
         if(this!=&other){
             close_udp_socket(socket);
             socket=other.socket;other.socket={};
-            peer=other.peer;peer_len=other.peer_len;other.peer_len=0;
+            peer=other.peer;other.peer={};
             keys=other.keys;
             session_id=other.session_id;other.session_id=0;
             generation=other.generation;other.generation=0;
