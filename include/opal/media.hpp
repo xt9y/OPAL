6c274388932920863d33a2037567ca61484ea799
xt9y <felix@xt9y.de>
2026-09-09 20:01:37 +0200

    refactor: add opaque sink wait handle

--- include/opal/media.hpp
+++ include/opal/media.hpp
@@ -38,11 +38,13 @@ struct SinkProcess {
         ProcessIoHandle io;
         ProcessIoHandle fd; // transitional source alias; not a POSIX type
     };
+    ProcessIoHandle wait_io = kInvalidProcessIoHandle;
     bool compact_input = false;
 
     constexpr SinkProcess() noexcept : process(kInvalidProcessHandle), io(kInvalidProcessIoHandle) {}
-    constexpr SinkProcess(ProcessNativeHandle native_process, ProcessIoHandle native_io, bool compact = false) noexcept
-        : process(native_process), io(native_io), compact_input(compact) {}
+    constexpr SinkProcess(ProcessNativeHandle native_process, ProcessIoHandle native_io, bool compact = false,
+                          ProcessIoHandle native_wait_io = kInvalidProcessIoHandle) noexcept
+        : process(native_process), io(native_io), wait_io(native_wait_io), compact_input(compact) {}
     bool valid() const noexcept { return process != kInvalidProcessHandle && io != kInvalidProcessIoHandle; }
 };
 
