96be4a69dc1e10c392c28f7e409c16eaf37d085e
xt9y <felix@xt9y.de>
2026-09-10 02:23:32 +0200

    feat: add host display manager

---
+++ include/opal/host_display.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <opal/display_backend.hpp>
+
+#include <memory>
+#include <string>
+
+namespace opal {
+
+class HostDisplayManager {
+public:
+    HostDisplayManager();
+    HostDisplayManager(const HostDisplayManager&) = delete;
+    HostDisplayManager& operator=(const HostDisplayManager&) = delete;
+    ~HostDisplayManager();
+
+    bool prepare(const StreamOptions& stream);
+    bool healthy() const;
+    void stop();
+
+    const DisplayTarget& target() const noexcept { return target_; }
+    std::string backend_name() const;
+    PlatformError last_platform_error() const;
+
+private:
+    std::unique_ptr<DisplayBackend> backend_;
+    DisplayTarget target_{};
+    PlatformError error_{};
+    bool active_ = false;
+};
+
+}
