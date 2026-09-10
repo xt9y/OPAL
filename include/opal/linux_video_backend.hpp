b04d2578d537426c0ff654a1a3505817fe75596b
xt9y <felix@xt9y.de>
2026-09-10 02:30:28 +0200

    feat: define Linux direct video backend factories

---
+++ include/opal/linux_video_backend.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+
+namespace opal {
+
+class CaptureBackend;
+class VideoEncoderBackend;
+
+std::unique_ptr<CaptureBackend> make_linux_pipewire_capture_backend(std::uint32_t node_id);
+std::unique_ptr<VideoEncoderBackend> make_linux_video_encoder_backend();
+
+}
