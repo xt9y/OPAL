1ce2752861cf5c6651b70ba4e8aad6105d417efd
xt9y <felix@xt9y.de>
2026-09-10 10:20:22 +0200

    refactor: pass display targets into capture backends

--- include/opal/capture_backend.hpp
+++ include/opal/capture_backend.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <opal/display_backend.hpp>
 #include <opal/media_profile.hpp>
 #include <opal/native_video_frame.hpp>
 #include <opal/platform_error.hpp>
@@ -13,8 +14,8 @@ namespace opal {
 class CaptureBackend {
 public:
     virtual ~CaptureBackend() = default;
-    virtual bool start(const StreamOptions &stream) = 0;
-    virtual bool next(NativeVideoFrame &frame, int timeout_ms) = 0;
+    virtual bool start(const StreamOptions& stream, const DisplayTarget* target = nullptr) = 0;
+    virtual bool next(NativeVideoFrame& frame, int timeout_ms) = 0;
     virtual void stop() = 0;
     virtual CaptureTimestampQuality timestamp_quality() const = 0;
     virtual std::string backend_name() const = 0;
