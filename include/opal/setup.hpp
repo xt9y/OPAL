bdcfcbd3cb68fefe0530cd22fd48bacacf798a04
xt9y <felix@xt9y.de>
2026-09-10 16:53:59 +0200

    fix: make restart role aware

--- include/opal/setup.hpp
+++ include/opal/setup.hpp
@@ -2,6 +2,7 @@
 #include <opal/media_profile.hpp>
 namespace opal {
 int interactive_run(const StreamOptions &stream={});
+int interactive_restart();
 int interactive_setup();
 int interactive_select();
 int interactive_remove();
