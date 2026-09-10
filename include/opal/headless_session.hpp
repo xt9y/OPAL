480cc95d05d7b1986de937fc9f942385b05aa93f
xt9y <felix@xt9y.de>
2026-09-10 02:28:09 +0200

    feat: add managed Linux desktop session interface

---
+++ include/opal/headless_session.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <opal/display_backend.hpp>
+
+#include <memory>
+#include <string>
+
+namespace opal {
+
+class HeadlessSession {
+public:
+    HeadlessSession();
+    HeadlessSession(const HeadlessSession&) = delete;
+    HeadlessSession& operator=(const HeadlessSession&) = delete;
+    ~HeadlessSession();
+
+    bool start(const DisplayMode& mode);
+    bool running() const;
+    void stop();
+
+    std::string wayland_display() const;
+    std::string last_error() const;
+
+private:
+    struct Impl;
+    std::unique_ptr<Impl> impl_;
+};
+
+}
