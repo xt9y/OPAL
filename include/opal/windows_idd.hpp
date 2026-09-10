de81c572050cec541f1aa7a404a0308054c8543e
xt9y <felix@xt9y.de>
2026-09-10 10:57:16 +0200

    feat: expose Windows virtual display status

---
+++ include/opal/windows_idd.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cstdint>
+
+namespace opal {
+
+struct WindowsIddStatus {
+    bool device_present = false;
+    bool adapter_ready = false;
+    bool monitor_active = false;
+    int width = 0;
+    int height = 0;
+    int refresh_hz = 0;
+};
+
+WindowsIddStatus windows_idd_status();
+bool windows_physical_display_present();
+
+}
