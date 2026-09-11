61345c5f25955ffc26b970f516659b17c5af9bb4
xt9y <felix@xt9y.de>
2026-09-11 20:11:05 +0200

    refactor: remove mutable duplicate topology state

--- include/opal/host_display.hpp
+++ include/opal/host_display.hpp
@@ -2,11 +2,8 @@
 
 #include <opal/display_backend.hpp>
 
-#include <atomic>
-#include <chrono>
 #include <memory>
 #include <string>
-#include <thread>
 
 namespace opal {
 
@@ -34,19 +31,10 @@ public:
     PlatformError last_platform_error() const;
 
 private:
-    void start_duplicate_hotplug_watch();
-    void stop_duplicate_hotplug_watch();
-
     std::unique_ptr<DisplayBackend> backend_;
     DisplayTarget target_{};
     PlatformError error_{};
-    HostDisplayMode requested_mode_ = HostDisplayMode::Duplicate;
-    std::atomic<bool> duplicate_watch_running_{false};
-    std::atomic<bool> physical_display_usable_{false};
-    std::thread duplicate_watch_thread_;
-    bool duplicate_layout_active_ = false;
     bool active_ = false;
-    std::chrono::steady_clock::time_point next_layout_check_{};
 };
 
 ActiveHostDisplay active_host_display();
